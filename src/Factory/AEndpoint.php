d268ced2b0c9d455909a9dfbf9570ffeef4f54b7
Alexei <a.doroshenko@autodoc.eu>
2023-03-17 15:51:25 +0200

    Build structure of the Vatlayer package

--- src/Factory/AEndpoint.php
+++ src/Factory/AEndpoint.php
@@ -2,16 +2,35 @@
 
 namespace AlxDorosenco\VatlayerForLaravel\Factory;
 
-use AlxDorosenco\VatlayerForLaravel\Http\Response;
+use AlxDorosenco\VatlayerForLaravel\Http\Client;
 
 abstract class AEndpoint
 {
     /**
-     * @return Response
+     * @var Client
      */
-    public function getResponse(): Response
+    private Client $client;
+
+    public function __construct()
     {
-        return new Response($this);
+        $this->client = new Client($this->getType(), $this->getData());
+    }
+
+    /**
+     * @return array
+     * @throws \JsonException
+     */
+    public function toArray(): array
+    {
+        return json_decode($this->client->getContents(), true, 512, JSON_THROW_ON_ERROR);
+    }
+
+    /**
+     * @return string
+     */
+    public function toJson(): string
+    {
+        return $this->client->getContents();
     }
 
     /**
--- src/Http/Response.php
+++ src/Http/Response.php
@@ -2,38 +2,7 @@
 
 namespace AlxDorosenco\VatlayerForLaravel\Http;
 
-use AlxDorosenco\VatlayerForLaravel\Factory\AEndpoint;
-
 class Response
 {
-    /**
-     * @var Client
-     */
-    private Client $client;
 
-    /**
-     * @param AEndpoint $endPoint
-     */
-    public function __construct(
-        private AEndpoint $endPoint
-    ){
-        $this->client = new Client($this->endPoint->getType(), $this->endPoint->getData());
-    }
-
-    /**
-     * @return array
-     * @throws \JsonException
-     */
-    public function toArray(): array
-    {
-        return json_decode($this->client->getContents(), true, 512, JSON_THROW_ON_ERROR);
-    }
-
-    /**
-     * @return string
-     */
-    public function toJson(): string
-    {
-        return $this->client->getContents();
-    }
 }
